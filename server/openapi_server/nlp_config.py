93f303ee3f00be001ded31eca9fa9a1ccce16567
Connor Boyle <connor.bo@gmail.com>
2021-09-03 15:15:46 -0700

    Fix Model Download
    We now use the `transformers` library's built-in downloader,
    instead of trying to get it from the web.

--- server/openapi_server/nlp_config.py
+++ server/openapi_server/nlp_config.py
@@ -3,19 +3,12 @@ from transformers import AutoModelForTokenClassification as amc, pipeline
 
 
 class Bert:
-    def __init__(self) -> None:
-        self.tokenizer = None
-        self.model = None
-        self.nlp = None
-
-    def initialize(self):
-        self.tokenizer = at.from_pretrained("./dslim-bert/tokenizer")
-        self.model = amc.from_pretrained("./dslim-bert/model")
+    def __init__(self):
+        self.tokenizer = at.from_pretrained("dslim-tokenizer")
+        self.model = amc.from_pretrained("dslim-model")
         self.nlp = pipeline("ner", model=self.model, tokenizer=self.tokenizer)
 
     def get_entities(self, sample_text, label):
-        if self.nlp is None:
-            self.initialize()
         ner_results = self.nlp(sample_text)
         ner_results = [r for r in ner_results if label in r['entity']]
         return ner_results
